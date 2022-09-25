d81835f59080ab717767a52b50ed166e577ab912
sanjay tomar <sanjaytomar717@gmail.com>
2022-09-25 17:13:17 +0530

    "all the extra files just to recap the learning"

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div className='about'>
+        <h1>This is a about page</h1>
+    </div>
+  )
+}
+
+export default About
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,12 +5,16 @@ function Navbar() {
   return (
     <div className='navbar'>
         <nav className="main_navbar">
+          <h1>Techfornerd</h1>
             <ul>
                 <li>Home</li>
                 <li>Blog</li>
                 <li>Contact</li>
                 <li>About</li>
             </ul>
+            <div className="nav_right">
+              <button id="subscribe_button">Subscribe</button>
+            </div>
         </nav>
     </div>
   )
